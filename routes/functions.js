4ec0a0153e6dba62bb749b097383c0c0d2729de9
unknown <Joshua.Lucier@gmail.com>
2016-06-24 17:30:41 -0400

    created auth function with callback

--- routes/functions.js
+++ routes/functions.js
@@ -87,6 +87,70 @@ module.exports = {
 
 	},
 
+	Auth: function(req,res,next,callback){
+		if(req.body.username && req.body.password){//if coming from /login
+			username = req.body.username;
+			password = req.body.password;
+		} else if (req.cookies.username && req.cookies.password){//if coming from other but already authenticated
+			username = req.cookies.username;
+			password = req.cookies.password;
+		} else {  //not authenticated or coming from /login need to redirect to login
+			res.cookie('page',req.originalUrl);
+			res.render('login',{title: 'Login'});
+			return;
+		}
+
+		var post_data = querystring.stringify({
+			accid: accid,
+			acckey: key,
+			cmd: 'authenticateMember',
+			memun: username,
+			mempw: password
+		});
+		var post_options = {
+			host: 'secure2.aladtec.com',
+			port: 443,
+			path: '/wrva/xmlapi.php',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded',
+				'Content-Length': Buffer.byteLength(post_data)
+			}
+		}
+		var post_req = https.request(post_options, function(post_res){
+			post_res.setEncoding('utf8');
+			post_res.on('data',function (chunk){
+				/*console.log("username: " + username + " password: " + password);
+				console.log("statusCode: " + post_res.statusCode);
+				console.log("headers: " + post_res.headers);
+				console.log('Response: ' + chunk);
+				*/
+				parseString(chunk, function(err, result){
+					if(result.results.errors)
+					{
+						console.log(result.results.errors[0].error[0]);
+						error = {};
+						error.stack = result.results.errors[0].error[0]['_'] + ' ID: ' + result.results.errors[0].error[0]['$'].id;
+						error.status = result.results.errors[0].error[0]['_'] + ' ID: ' + result.results.errors[0].error[0]['$'].id;
+						res.render('error',{title: 'Error', error: error});
+						return;
+					}
+					console.log(result.results.authentication[0]['$'].message);
+					if(result.results.authentication[0]['$'].code==0) 
+					{
+						res.cookie('username',username);
+						res.cookie('password',password);
+						callback(req,res,next);
+					}
+					else res.render('invalid',{title: 'title', message: result.results.authentication[0]['$'].message});
+				});
+			});
+		});
+		
+		post_req.write(post_data);
+		post_req.end();
+	},
+
 	AdminAuthenticate: function(page,data,req,res){
 		if(req.body.username && req.body.password){//if coming from /login
 			username = req.body.username;
