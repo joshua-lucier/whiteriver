2f41271cc93bbf734b0b2c63a25f3e1e593d4f53
unknown <Joshua.Lucier@gmail.com>
2016-07-29 12:13:35 -0400

    changed check marks

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -17,13 +17,3 @@ $("body").ready(function(){
 			$(this).next().remove();
 	}); 
 });
-
-$('input[type="radio"] + span::before').html('<img src="/images/emptycheck.png');
-$('input[type="radio"]:checked + span::before').html('<img src="/images/checked.png">');
-
-$('input[type="radio"]').click(function(){
-	$('input[type="radio"] + span::before').html('<img src="/images/emptycheck.png');
-	if($($(this)).is(':checked')) {
-
-	}
-});
